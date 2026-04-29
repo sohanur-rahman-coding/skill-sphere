6b246569ae10008201b11fa5517a6c38f1a755ea
sohanbd413-prog <sohanbd413@gmail.com>
2026-04-30 04:51:35 +0600

    redeployment

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,6 +1,6 @@
 
 export const PopularData = async () => {
-  const res = await fetch("http://localhost:3000/data.json");
+  const res = await fetch("http://localhost:3000/data.json",{cache:'no-store'});
   const data = await res.json();
   return data;
 };
