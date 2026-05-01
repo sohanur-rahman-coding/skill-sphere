8d51ee4b265141e1c7a1898bf8596d1eef0b0232
sohanbd413-prog <sohanbd413@gmail.com>
2026-05-01 07:44:20 +0600

    changing base

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://skill-sphere-six-ruddy.vercel.app/"
 })
 export const { signIn, signUp,signOut, useSession } = createAuthClient()  
